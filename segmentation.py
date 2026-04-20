38e8e93d9aaf8d094339252918e0eb8eccb472af
Aashish-gthb <ashujaat123987@gmail.com>
2026-04-20 23:36:16 +0530

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def load_and_clean(filepath="data/OnlineRetail.csv"):
     df = pd.read_csv(filepath, encoding='latin-1')
-    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])  # 👈 add this line
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])  
     df.dropna(subset=['CustomerID'], inplace=True)
     df = df[~df['InvoiceNo'].astype(str).str.startswith('C')]
     df = df[(df['Quantity'] > 0) & (df['UnitPrice'] > 0)]
@@ -10,17 +10,21 @@ def load_and_clean(filepath="data/OnlineRetail.csv"):
     return df
 
     return df
+
+
 def build_rfm(df):
     snapshot_date = df['InvoiceDate'].max() + pd.Timedelta(days=1)
 
     rfm = df.groupby('CustomerID').agg({
-        'InvoiceDate': lambda x: (snapshot_date - x.max()).days,  # Recency
-        'InvoiceNo':   'nunique',                                  # Frequency
-        'TotalPrice':  'sum'                                       # Monetary
+        'InvoiceDate': lambda x: (snapshot_date - x.max()).days,  
+        'InvoiceNo':   'nunique',                                  
+        'TotalPrice':  'sum'                                       
     }).reset_index()
 
     rfm.columns = ['CustomerID', 'Recency', 'Frequency', 'Monetary']
     return rfm
+
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
@@ -43,6 +47,8 @@ def apply_kmeans(rfm, n_clusters=4):
     rfm['Segment'] = rfm['Cluster'].map(segment_map)
 
     return rfm
+
+
 import matplotlib.pyplot as plt
 
 def plot_elbow(rfm_scaled):
